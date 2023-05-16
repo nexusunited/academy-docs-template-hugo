bf15acf5d5b3c7c891b81ae1f5913741ec47c36b
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-05-16 15:43:06 +0200

    Init

---
+++ assets/js/home.js
@@ -0,0 +1,38 @@
+function switchHomeLayout() {
+  const pageDiv = document.getElementById("page");
+  const profileDiv = document.getElementById("profile");
+  const layoutCode = document.querySelectorAll("code[id=layout]");
+  if (pageDiv.style.display === "none") {
+    pageDiv.style.display = "block";
+    profileDiv.style.display = "none";
+    layoutCode.forEach(function (el) {
+      el.innerText = "page";
+    });
+  } else {
+    pageDiv.style.display = "none";
+    profileDiv.style.display = "block";
+    layoutCode.forEach(function (el) {
+      el.innerText = "profile";
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll("#switch-layout-button").forEach((button) =>
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      switchHomeLayout();
+    })
+  );
+});
+
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+  accordionHeaders.forEach((header) => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    content.classList.toggle('hidden');
+  });
+});
+
+
---
+++ content/_index.md
@@ -0,0 +1,14 @@
+---
+title: "Welcome to Congo!!!!! :tada:"
+description: "This is a demo of the Congo theme for Hugo."
+draft: false
+slug: "icons"
+tags: ["icons", "sample", "shortcodes"]
+showDateUpdated: true
+---
+
+
+{{< lead >}}
+A powerful, lightweight theme for Hugo built with Tailwind CSS.
+{{< /lead >}}
+
